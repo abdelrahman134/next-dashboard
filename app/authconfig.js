a57e1d2aefb1d772b07896acb24f6a2e3753e53f
abdelrahman134 <104602156+abdelrahman134@users.noreply.github.com>
2024-02-08 11:34:00 +0200

    first commit

--- app/authconfig.js
+++ app/authconfig.js
@@ -6,7 +6,11 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       const isLoggedIn = auth?.user;
-      const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard");
+
+      const isOnDashboard =
+      
+        request.nextUrl.pathname.startsWith("/dashboard");
+  
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false;
--- app/lib/action.js
+++ app/lib/action.js
@@ -150,10 +150,11 @@ export const authenticate =async (prevState,formData)=>{
  try {
  
    await signIn("credentials", { username, password });
+
  } catch (err) {
-//    if (err.message.includes("CredentialsSignin")) {
-     return "Wrong Credentials";
-   
+    if (err.message.includes("CredentialsSignin")) {
+      return "Wrong Credentials";
+    }
    throw err;
  }
 }
