383d1824da3599152d40815cdc8f776eddf6220c
abdelrahman134 <104602156+abdelrahman134@users.noreply.github.com>
2024-02-11 18:24:31 +0200

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,26 @@
-"use client"
+// "use client"
 import Image from 'next/image'
 import { auth } from './auth'
 import { redirect } from 'next/navigation'
 import { useRouter } from 'next/navigation'
-
-export default async function  Home() {
-  const route = useRouter()
-  if(true) return route.push("/login")
+import Link from 'next/link';
+import styles from "./ui/home.module.css"
+export default function  Home() {
+  // const route = useRouter()
+  // if(true) return route.push("/login")
   return (
-   <h2>Homepage</h2>
-  )
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+      <h2 className={styles.title}>Homepage</h2>
+        
+
+      <Link className={styles.link} href="/login">
+        Login
+      </Link>
+      <Link className={styles.link} href="/dashboard">
+        Dashboard
+      </Link>
+      </div>
+    </div>
+  );
 }
