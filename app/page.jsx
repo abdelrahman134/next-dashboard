a4fcdda3a5e938717e9119271887128aa3e38b4f
abdelrahman134 <104602156+abdelrahman134@users.noreply.github.com>
2024-02-11 18:02:44 +0200

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,10 @@
+"use client"
 import Image from 'next/image'
+import { auth } from './auth'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
+export default async function  Home() {
+  if(true)  redirect("/login")
   return (
    <h2>Homepage</h2>
   )
