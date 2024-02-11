d3f42d940dbbcfdafecff772644a74528fcc48b2
abdelrahman134 <104602156+abdelrahman134@users.noreply.github.com>
2024-02-11 20:08:58 +0200

    first commit

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,21 +1,24 @@
 import Product from "./models/Product"
 import User from "./models/User"
 import { connectToDB } from "./utils"
-export const fetchUsers = async(q,page)=>{
-    const regex = new RegExp(q,"i")
-    const ITEM_PER_PAGE=2
-    try{
-        connectToDB()
-        const count =await User.find({username :{$regex :regex}}).count()  
-        const users =await User.find({username:{$regex:regex}}).limit(ITEM_PER_PAGE).skip(ITEM_PER_PAGE*(page-1))
-       
-        return {count,users}
-    }
-    catch(err){
-        throw new Error ("Failed to fetch User")
-    }
+export const fetchUsers = async (q, page) => {
+  const regex = new RegExp(q, "i");
 
-}
+  const ITEM_PER_PAGE = 2;
+
+  try {
+    connectToDB();
+    const count = await User.find({ username: { $regex: regex } }).count();
+    const users = await User.find({ username: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
+    return { count, users };
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch users!");
+  }
+};
+
 export const fetchUser = async (id) => {
 
   try {
@@ -38,16 +41,18 @@ export const fetchProduct = async (id) => {
 };
 export const fetchProducts = async (q, page) => {
   const regex = new RegExp(q, "i");
+
   const ITEM_PER_PAGE = 2;
+
   try {
     connectToDB();
     const count = await Product.find({ title: { $regex: regex } }).count();
     const products = await Product.find({ title: { $regex: regex } })
       .limit(ITEM_PER_PAGE)
       .skip(ITEM_PER_PAGE * (page - 1));
-
     return { count, products };
   } catch (err) {
-    throw new Error("Failed to fetch User");
+    console.log(err);
+    throw new Error("Failed to fetch products!");
   }
 };
